759163ec3bfbf679162d02a37ae072afeaafa42a
TianNyAvo <tiannyavo.nexthope@gmail.com>
2023-09-27 15:25:49 +0300

    remove useless console.log

--- src/modules/users/users.services.js
+++ src/modules/users/users.services.js
@@ -3,12 +3,10 @@ const {UserLogin} = require("../../generated_pb/user_pb");
 
 exports.login = async (user) => {
   console.log("invoking login");
-  console.log("email and password: ", user.email, user.password);
 
   const req =  new UserLogin()
     .setEmail(user.email)
     .setPassword(user.password);
-  console.log("req: ", req.getEmail(), req.getPassword());
 
   return new Promise((resolve, reject) => {
     clientGrpc.getUserInstance().login(req, (err, res) => {
