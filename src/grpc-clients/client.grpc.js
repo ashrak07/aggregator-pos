2bfe51be2e70f5237ad05534b9fca40c84a99847
DeveloperBusinessSolution <tahina@nexthope.net>
2023-11-28 12:47:49 +0000

    update client.grpc and .env

--- src/grpc-clients/client.grpc.js
+++ src/grpc-clients/client.grpc.js
@@ -10,7 +10,7 @@ const {TicketClient} = require("../generated_pb/ticket_grpc_pb");
 
 exports.getUserInstance = () => {
   if (this.userClient == null) {
-    this.userClient = new UserServiceClient(`node_ms-user:${process.env.MS_USER_GRPC_PORT}`, grpc.credentials.createInsecure());
+    this.userClient = new UserServiceClient(`${process.env.MS_USER_CONTAIER_NAME}:${process.env.MS_USER_GRPC_PORT}`, grpc.credentials.createInsecure());
   }
   return this.userClient;
 
@@ -18,35 +18,35 @@ exports.getUserInstance = () => {
 
 exports.getShopInstance = () => {
   if (this.shopClient == null) {
-    this.shopClient = new ShopServiceClient(`node_ms-shop:${process.env.MS_SHOP_GRPC_PORT}`, grpc.credentials.createInsecure());
+    this.shopClient = new ShopServiceClient(`${process.env.MS_SHOP_CONTAINER_NAME}:${process.env.MS_SHOP_GRPC_PORT}`, grpc.credentials.createInsecure());
   }
   return this.shopClient;
 }
 
 exports.getEventInstance = () => {
   if (this.eventClient == null) {
-    this.eventClient = new EventClient(`${process.env.MS_EVENT_IP}:${process.env.MS_EVENT_GRPC_PORT}`, grpc.credentials.createInsecure());
+    this.eventClient = new EventClient(`${process.env.MS_EVENT_CONTAINER_NAME}:${process.env.MS_EVENT_GRPC_PORT}`, grpc.credentials.createInsecure());
   }
   return this.eventClient;
 }
 
 exports.getOrderInstance = () => {
   if (this.orderClient == null) {
-    this.orderClient = new OrderClient(`${process.env.MS_ORDER_IP}:${process.env.MS_ORDER_GRPC_PORT}`, grpc.credentials.createInsecure());
+    this.orderClient = new OrderClient(`${process.env.MS_ORDER_CONTAINER_NAME}:${process.env.MS_ORDER_GRPC_PORT}`, grpc.credentials.createInsecure());
   }
   return this.orderClient;
 }
 
 exports.getOrderlineInstance = () => {
   if (this.orderlineClient == null) {
-    this.orderlineClient = new OrderlineClient(`${process.env.MS_ORDER_IP}:${process.env.MS_ORDER_GRPC_PORT}`, grpc.credentials.createInsecure());
+    this.orderlineClient = new OrderlineClient(`${process.env.MS_ORDER_CONTAINER_NAME}:${process.env.MS_ORDER_GRPC_PORT}`, grpc.credentials.createInsecure());
   }
   return this.orderlineClient;
 }
 
 exports.getTicketInstance = () => {
   if (this.ticketClient == null) {
-    this.ticketClient = new TicketClient(`node_ms-ticket:${process.env.MS_TICKET_GRPC_PORT}`, grpc.credentials.createInsecure());
+    this.ticketClient = new TicketClient(`${process.env.MS_TICKET_CONTAINER_NAME}:${process.env.MS_TICKET_GRPC_PORT}`, grpc.credentials.createInsecure());
   }
   return this.ticketClient;
 }
